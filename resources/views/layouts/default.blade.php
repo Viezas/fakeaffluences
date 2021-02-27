a8813d1d2d9651943dcada79d168bdb4b3db6ab9
Viezas <dxviezas@gmail.com>
2021-02-27 21:40:21 +0100

    css fix
    fixing css errors

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $pageTitle ;?></title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link href="{{url('/assets/css/style.css')}}" rel="stylesheet">
+  <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 <body>
   @include('_partials.banner')
