bc9b92d6ff436a5b111fd0c8cf288b2279195ecd
Viezas <dxviezas@gmail.com>
2021-03-08 10:34:45 +0100

    Update information.php

--- config/information.php
+++ config/information.php
@@ -23,18 +23,18 @@ class information
     {
       return [
         'name' => 'V.Hive',
-        'description' => "
-          Le V.Hive est un bâtiment aux multiples facettes.<br> 
-          Servant avant tout de base pour l'équipe e-sportive Vitality, il est également ouvert au public et propose de nombreux services.<br>
+        'description' => '
+          Le V.Hive est un bâtiment aux multiples facettes.<br>
+          Servant avant tout de base pour l\'équipe e-sportive Vitality, il est également ouvert au public et propose de nombreux services.<br>
           Parmi ses nombreux services, on peut citer entre autres : 
           <ul>
-            <li>La présence d'un café</li>
-            <li>Une boutique où l'on peut retrouver des produits dérivés de la Team Vitality (maillots, casquettes, ...)</li>
+            <li>La présence d\'un café</li>
+            <li>Une boutique où l\'on peut retrouver des produits dérivés de la Team Vitality (maillots, casquettes, ...)</li>
             <li>Des salles équipées pour le gaming ainsi que des salles prévues pour des Bootcamp</li>
           </ul>
           Le V.Hive est donc un lieu de rencontre et de rapprochement entre la Team Vitality et sa communauté.<br>
           En plus des services énumérés ci-dessus, divers événements sont organisés au sein du V.Hive (soirée de présentation, événements nationaux, master classes, ...)
-          ",
+          ',
         'adress' => '102 Boulevard de Sébastopol, 75003 Paris',
         'googlemapsLocation' => [
           'lat' => 49.089602278815796,
